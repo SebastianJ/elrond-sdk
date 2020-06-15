c118c1e740053b0f7f810ed65851e159c0e04063
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-06-15 18:39:55 +0200

    Fix broken nonce retries

--- transactions/transactions.go
+++ transactions/transactions.go
@@ -34,8 +34,14 @@ func SendTransaction(
 	if txError != nil {
 		// If we've sent an invalid nonce - sleep 3 seconds and then retry again using a fresh nonce
 		if strings.Contains(txError.Error(), "transaction generation failed: invalid nonce") {
-			time.Sleep(3 * time.Second)
-			return SendTransaction(wallet, receiver, amount, sendMaximumAmount, nonce, txData, gasParams, client)
+			time.Sleep(1 * time.Second)
+
+			currentNonce, err := getNonce(client, wallet.Address, -1)
+			if err != nil {
+				return "", err
+			}
+
+			return SendTransaction(wallet, receiver, amount, sendMaximumAmount, int64(currentNonce), txData, gasParams, client)
 		}
 
 		return "", txError
