1b59e4239ba6f83fe95cd80ba3787209d18d67dc
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-06-16 11:57:50 +0200

    Don't include the metashard in the shard count

--- transactions/addresses_test.go
+++ transactions/addresses_test.go
@@ -12,13 +12,13 @@ import (
 func TestCalculateShardForAddress(t *testing.T) {
 	t.Parallel()
 
-	numberOfShards := uint32(3)
+	numberOfShards := uint32(2)
 
 	tests := []struct {
 		address string
 		shardID uint32
 	}{
-		{address: "erd1py99xhmfs4js6s6x0q6hp3c6zqwpq0fy4d3qmrwh7su68rudz2wq4e432r", shardID: 0},
+		{address: "erd10j8smvel8k7amn53hu0tetvwscudjltgq7znrjgj7mp4xdydeeaqajjvwy", shardID: 0},
 		{address: "erd1trvgjh6c77j58wwqd5t7exdhxm2gsw6kwkf0xuzh0pza743jcjqqx0u87l", shardID: 0},
 		{address: "erd1ffpa2ue77g50r4arz3rmqkxj3xykw4vgx7hyuxds9mc27ts97rtspfa6px", shardID: 1},
 		{address: "erd1j6tzkx3dn2pu67tj83fuvgv4jnpsm3ehrw758lspkv9d03xjvpgs09trr4", shardID: 1},
