d24950fb89c1c59ae47c6eea0a10380f9db502e2
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-06-22 16:49:02 +0200

    Add deps.sh + update dependencies

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/SebastianJ/elrond-sdk
 go 1.14
 
 require (
-	github.com/ElrondNetwork/elrond-go v1.0.128
+	github.com/ElrondNetwork/elrond-go v1.0.133-0.20200622120235-de0b2125990e
 	github.com/ethereum/go-ethereum v1.9.14
 	github.com/json-iterator/go v1.1.6 // indirect
 	github.com/pkg/errors v0.9.1
---
+++ scripts/deps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Updating dependencies"
+
+deps=(
+ElrondNetwork/elrond-go
+)
+
+for dep in "${deps[@]}"; do
+  commit=$(curl -Ls https://api.github.com/repos/${dep}/commits/master | jq '.sha' | tr -d '"')
+  echo "Updating dependency ${dep} to latest master commit ${commit}"
+  go get -u github.com/${dep}@${commit}
+done
+
+echo "Tidying up go.mod ..."
+go mod tidy
